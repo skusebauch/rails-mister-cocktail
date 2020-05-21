5fffebb5f27690728647b5795f9a9fb86935a185
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-05-21 12:50:20 +0200

    setup entire models and dependecies

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,2 +1,7 @@
 class Cocktail < ApplicationRecord
+  has_many :doses
+  has_many :ingredients, through: :doses, dependent: :destroy
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,7 @@
 class Ingredient < ApplicationRecord
+  has_many :cocktails
+  has_many :doses
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
 end
