cf2a90dbbdaf3af829adc36e2c8dd214970e4ac5
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-05-21 15:01:58 +0200

    implement the cocktail controller and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,7 @@
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
+
+Cocktail.create(name: "German Killer")
+Cocktail.create(name: "Just one")
+Cocktail.create(name: "Driver friendly")
