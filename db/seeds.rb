ba15fa41db00aaf0ed64537374552f5a86de1ddf
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-05-23 19:28:24 +0200

    prepared for deployment

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,8 +26,20 @@ end
 
 puts "Create three default cocktails"
 
-Cocktail.create(name: "German Killer")
-Cocktail.create(name: "Just one")
-Cocktail.create(name: "Driver friendly")
+url1 = URI.open('https://res.cloudinary.com/dgwgea5k3/image/upload/v1590254625/blue-margarita-with-lemon-2480828_hnta22.jpg')
+url2 = URI.open('https://res.cloudinary.com/dgwgea5k3/image/upload/v1590254555/alcoholic-bar-beverage-blur-338713_tr4ljk.jpg')
+url3 = URI.open('https://res.cloudinary.com/dgwgea5k3/image/upload/v1590254707/a-drink-in-a-glass-with-green-leaf-2691360_cpcr6v.jpg')
+
+german_killer = Cocktail.new(name: "German Killer")
+one_shot = Cocktail.new(name: "Just one")
+driver_friendly = Cocktail.new(name: "Driver friendly")
+
+german_killer.photo.attach(io: url1, filename: "german_killer.jpg", content_type: "image/jpg")
+one_shot.photo.attach(io: url2, filename: "one_shot.jpg", content_type: "image/jpg")
+driver_friendly.photo.attach(io: url3, filename: "driver_friendly.png", content_type: "image/png")
+
+german_killer.save
+one_shot.save
+driver_friendly.save
 
 puts "Completed!"
